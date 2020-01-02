a5c457e195a790c5c1ccf9d122a554dec8a5c05d
Matt Miller <mmiller@tableau.com>
2020-01-02 15:47:59 -0500

    moved logging level down to debug.

--- setup_logging.py
+++ setup_logging.py
@@ -4,7 +4,7 @@ import logging
 
 #Set up logging
 logging.basicConfig(filename = "scrape.log",
-					level = logging.INFO,
+					level = logging.DEBUG,
 					format='%(levelname)s - %(asctime)s - %(funcName)s: %(message)s',
     				datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger()

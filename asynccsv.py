b4d4b3864bedf24ea38145b93619babb90b5f962
Matt Miller <mattmiller87@gmail.com>
2020-10-02 15:34:42 -0400

    Update asynccsv.py
    update to latest season 15

--- asynccsv.py
+++ asynccsv.py
@@ -24,7 +24,7 @@ class csvIO:
 		self.csvoutput = results.output
 		self.seasons = results.seasons
 		self.playlists = results.playlists
-		self.latestseason = '14' # need a better way to update this, perhaps dynamically?
+		self.latestseason = '15' # need a better way to update this, perhaps dynamically?
 		self.header = []
 
 	def areadCSVLinks(self):
